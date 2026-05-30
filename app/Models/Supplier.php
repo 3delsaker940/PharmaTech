07732372933ee6febaac412aa7fe4b0574471879
selenajiro <selena.jiro@gmail.com>
2026-05-30 19:56:38 +0300

    feat: add relationships to Pharmacy.php

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Supplier extends Model
 {
+    protected $table = 'suppliers';
     protected $guarded = [];
 
     public function pharmacy(): BelongsTo
