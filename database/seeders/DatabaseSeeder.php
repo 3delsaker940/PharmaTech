b44a3d9a962809e64cd284676555224bcd0abdf0
3delsaker940 <3delsaker940@gmail.com>
2026-06-02 02:46:50 +0300

    chore: mark email as verified in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,7 @@ class DatabaseSeeder extends Seeder
                 'status' => 'active',
             ]
         );
+        $user->MarkEmailAsVerified();
 
         Pharmacy::firstOrCreate(
             ['id' => 1],
