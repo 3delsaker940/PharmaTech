aa13d37ca723f744a112d3485d9159418c1928a3
3delsaker940 <3delsaker940@gmail.com>
2026-08-18 22:02:24 +0300

    style: format and clean up code

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+
+class SuperAdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $role = Role::firstOrCreate(['name' => 'system_admin', 'guard_name' => 'web']);
+
+        $user = User::firstOrCreate(
+            ['email' => 'admin@pharmatech.com'],
+            [
+                'first_name' => 'Super',
+                'last_name'  => 'Admin',
+                'password'   => Hash::make('Password321'),
+                'status'     => 'active',
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $user->assignRole($role);
+    }
+}
